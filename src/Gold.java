a05637ba3abb23e58fae3070e644c09553dfb647
Sabrina Fang <sabrinalfang@gmail.com>
2022-04-18 17:27:49 -0400

    Created class 'Object' and 'Gold'

---
+++ src/Gold.java
@@ -0,0 +1,10 @@
+public class Gold extends Object {
+
+    Gold(){
+        this.x = 300;
+        this.y = 500;
+        this.width =;
+        this.height =;
+        this.img = Toolkit.getDefaultToolkit().getImage("";)
+    }
+}
