9e02e64cbe13d5fdf9576a9e2972f024de2dc8cb
Sabrina Fang <sabrinalfang@gmail.com>
2022-04-18 13:15:48 -0400

    Motion of the line

--- src/gameWindow.java
+++ src/gameWindow.java
@@ -16,6 +16,17 @@ public class gameWindow extends JFrame {
         this.setTitle("Gold Mine");
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        while (true){
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
     public void paint (Graphics g){
--- src/line.java
+++ src/line.java
@@ -1,4 +1,13 @@
 import java.awt.*;
+
+/**
+ * Find the position of the ending point of the line through trig
+ * because it will be a right triangle
+ * a = angle
+ * endx = x + length*cos a
+ * endy = y + length*sin a 
+ */
+
 public class line {
 
     //starting position
@@ -9,8 +18,24 @@ public class line {
     int endx =500;
     int endy =500;
 
+    //length
+    double length = 100;
+    double a = 0;
+
+    //direction
+    int direction = 1;
+
     void paintSelf(Graphics g){
-        g.setColor(Color.red);
+        if (a<0.1) {
+            direction = 1;}
+        else if (a>0.9){
+            direction = -1;}
+
+        a = a + 0.005*direction;
+        endx = (int) (x + length*Math.cos(a*Math.PI));
+        endy = (int) (y + length*Math.sin(a*Math.PI));
+
+        g.setColor(Color.black);
         g.drawLine(x, y, endx, endy);
     }
 }
